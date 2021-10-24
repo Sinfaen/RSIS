8ed4667a45434057c3a10a7abb2f801dff05c136
Kelly Schultz <ksjaculus@gmail.com>
2021-10-23 21:15:05 -0400

    Add normal distribution sampling onto input signal, remove cargo.lock from git tracking

--- tst/models/sensor_rust/src/lib.rs
+++ tst/models/sensor_rust/src/lib.rs
@@ -14,13 +14,16 @@ extern crate memoffset;
 
 use modellib::BaseModel;
 
+use rand::distributions::Distribution;
+use statrs::distribution::Normal;
+
 mod heightSensor_interface;
 use heightSensor_interface::height_sensor;
 
 impl BaseModel for height_sensor {
     fn config(&mut self) -> bool {
         if self.params.limits[1] < self.params.limits[0] {
-            println!("Limit range must be specificed as [lower, upper]");
+            println!("Limit range must be specified as [lower, upper]");
             return false
         }
         true
@@ -29,6 +32,9 @@ impl BaseModel for height_sensor {
         self.config()
     }
     fn step(&mut self) -> bool {
+        let dist = Normal::new(0.0, self.params.noise).unwrap();
+        let mut r = rand::thread_rng();
+        self.data.measurement = self.inputs.signal + dist.sample(&mut r);
         if self.data.measurement < self.params.limits[0] ||
             self.data.measurement > self.params.limits[1]
         {
